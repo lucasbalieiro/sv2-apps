c69a5c314357007a579a44fad47ed2575caf69e8
bit-aloo <sshourya17@gmail.com>
2026-03-28 19:02:21 +0530

    complete handshake when authorize message is received even in cached messages

--- miner-apps/translator/src/lib/sv1/sv1_server/mod.rs
+++ miner-apps/translator/src/lib/sv1/sv1_server/mod.rs
@@ -1,3 +1,5 @@
+use stratum_apps::stratum_core::sv1_api::{json_rpc, Message};
+
 pub(super) mod channel;
 mod difficulty_manager;
 pub mod downstream_message_handler;
@@ -6,3 +8,12 @@ pub mod sv1_server;
 /// Delimiter used to separate original job ID from keepalive mutation counter.
 /// Format: `{original_job_id}#{counter}`
 const KEEPALIVE_JOB_ID_DELIMITER: char = '#';
+
+/// Check if Sv1 message is mining.authorize
+fn is_mining_authorize(msg: &Message) -> bool {
+    if let json_rpc::Message::StandardRequest(r) = &msg {
+        r.method == "mining.authorize"
+    } else {
+        false
+    }
+}
